9a443de5b5b43c72bd82caa845505c5c7809d81f
Linus Andersson <linus@b-77-47.eduroam.liu.se>
2010-11-09 11:35:36 +0100

    networkcomponent igen

--- client/src/networkcomponent.py
+++ client/src/networkcomponent.py
@@ -1,9 +1,10 @@
+# coding=utf8
 '''
 Created on 8 nov 2010
 
 @author: linus
 '''
-#coding: utf-8
+
 import socket
 import threading
 import time
@@ -18,16 +19,20 @@ class Threadednetwork(threading.Thread):
         threading.Thread.__init__ ( self )
         
     def run(self):
-        print 'Connected by', self.addr
+        #skriver ut anslutningen
+        #print 'Connected by', self.addr
+        #tar emot data
         data = self.conn.recv(1024)
-        
+        #splitar datat
         spliteddata = data.split('<>')
         data = spliteddata[0]
         datatype = spliteddata[1]
-        print data+' '+datatype
+
+        #skrev ut datan förut
+        #print data+' '+datatype
         self.conn.close()
 
-    
+#     
 class NetworkServer(threading.Thread):
     
     def __init__(self):
@@ -49,21 +54,21 @@ class NetworkServer(threading.Thread):
             Threadednetwork(conn, addr).start()    
 
 
-class Main():
     
-    def serverStart(self):
-        NetworkServer().start()
-    
-    
-    # funktion for att skicka data over natverket
-    def send(destination,package,type):
-        HOST = destination    # The remote host
-        PORT = 50011          # The same port as used by the server
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((HOST, PORT))
-        paket=package+'<>'+type
-        s.send(paket)
-        #data = s.recv(1024)
-        s.close()
-        #print 'Received', repr(data)
+#funktion for att starta igang servern
+def serverStart():
+    NetworkServer().start()
+
+
+# funktion for att skicka data over natverket
+def send(destination,package,type):
+    HOST = destination    # The remote host
+    PORT = 50011          # The same port as used by the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    paket=package+'<>'+type
+    s.send(paket)
+    #data = s.recv(1024)
+    s.close()
+    #print 'Received', repr(data)
     
