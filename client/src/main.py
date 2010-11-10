734d0003c187d1a4f8e0d93964ef783b62d589b3
Linus Andersson <linus@b-77-216.eduroam.liu.se>
2010-11-10 15:26:55 +0100

    main för client

---
+++ client/src/main.py
@@ -0,0 +1,10 @@
+'''
+Created on 10 nov 2010
+
+@author: linus
+'''
+import gui_views_test
+from gui_views_test import *
+import threading
+
+gui_views_test.GuiMain().start()
---
+++ client/src/test.py
@@ -0,0 +1,17 @@
+#coding=utf8
+'''
+Created on 9 nov 2010
+
+@author: linus
+'''
+
+from networkcomponent import *
+import networkcomponent
+import time
+
+
+#networkcomponent.serverStart()
+
+#time.sleep(3)
+
+networkcomponent.send('130.236.216.31','fifan','d')
