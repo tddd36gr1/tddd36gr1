d690533c2039cab1c9afe09bd5a3952a05b6988a
Alek <alek.astrom@gmail.com>
2010-11-11 13:50:29 +0100

    Dokumenterat push.py

--- server/src/push.py
+++ server/src/push.py
@@ -4,13 +4,13 @@ def pushStart():
     from class_.base_objects import Mission, StatusCode
     import networkcomponent
     import db
-   
-    #networkcomponent.send('127.0.0.1', Employee('aasdasd', 'f', 'asd'), 'fail')
 
     ip = '127.0.0.1'
     
+    #Send all status codes
     for statuscode in db.get_all(StatusCode):
         networkcomponent.send(ip,statuscode,'dbObject')
         
+    #Send all missions
     for mission in db.get_all(Mission):
         networkcomponent.send(ip,mission,'dbObject')
