0ef825dcbff9911003f5c4f4dee991f7ddcbed51
Alek <alek.astrom@gmail.com>
2010-11-11 15:37:50 +0100

    småfix i databasen

--- server/src/push.py
+++ server/src/push.py
@@ -5,12 +5,12 @@ def pushStart():
     import networkcomponent
     import db
 
-    ip = '127.0.0.1'
+    ip = '192.168.2.15'
     
     #Send all status codes
     for statuscode in db.get_all(StatusCode):
-        networkcomponent.send(ip,statuscode,'dbObject')
+        networkcomponent.send(ip,statuscode,'db_add')
         
     #Send all missions
     for mission in db.get_all(Mission):
-        networkcomponent.send(ip,mission,'dbObject')
+        networkcomponent.send(ip,mission,'db_add')
