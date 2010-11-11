5ae34154375ee73ade0c021ade2445ecc5271dc6
maemo <maemo@maemo-desktop.(none)>
2010-11-11 16:48:21 +0100

    det brinner på kartan

--- client/src/kartkomponent/gui.py
+++ client/src/kartkomponent/gui.py
@@ -5,6 +5,9 @@ import gobject
 import gui_map
 import data_storage
 import map_xml_reader
+from  class_.base_objects import Mission
+import db
+
 
 
 class Gui():
@@ -149,9 +152,10 @@ map.set_focus(15.5726, 58.4035)
 map.add_object("Employee1", data_storage.MapObject({"longitude":15.5629,
                                                    "latitude":58.4093},
                                                   "ikoner/employee.png"))
-#map.add_object("Mission1", data_storage.MapObject({"longitude":db.get_one(Mission).long,
-#                                                 "latitude":db.get_one(Mission).lat},
-#                                                   "ikoner/Mission.png"))
+db.add(Mission('Rädda katten i trädet', 15.578, 58.4048, 0.0, 2))
+map.add_object("Mission1", data_storage.MapObject({"longitude":db.get_one(Mission).long,
+                                                 "latitude":db.get_one(Mission).lat},
+                                                   "ikoner/Mission.png"))
 #map.add_object("Shape1", data_storage.MapObject({"longitude":15.5829,
 #                                                 "latitude":58.4093},
 #                                                "arc(x - 5, y - 5, 10, 0, 2 * math.pi)",
