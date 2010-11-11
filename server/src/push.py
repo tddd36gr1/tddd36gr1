f4e691c1f4fdf561d112016c824b93d6f7538af9
Alek <alek.astrom@gmail.com>
2010-11-11 13:43:51 +0100

    rensning på push

--- server/src/push.py
+++ server/src/push.py
@@ -1,14 +1,16 @@
 #coding=utf8
 
 def pushStart():
-    from class_.base_objects import Employee, Mission, StatusCode
+    from class_.base_objects import Mission, StatusCode
     import networkcomponent
     import db
    
     #networkcomponent.send('127.0.0.1', Employee('aasdasd', 'f', 'asd'), 'fail')
+
+    ip = '127.0.0.1'
     
     for statuscode in db.get_all(StatusCode):
-        networkcomponent.send('127.0.0.1',statuscode,'dbObject')
+        networkcomponent.send(ip,statuscode,'dbObject')
         
     for mission in db.get_all(Mission):
-        networkcomponent.send('127.0.0.1',mission,'dbObject')
+        networkcomponent.send(ip,mission,'dbObject')
