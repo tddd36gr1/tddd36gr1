dc69577352d18e6e8fbee2b5ccd97f2afa65dc80
maemo <maemo@maemo-desktop.(none)>
2010-11-11 14:44:13 +0100

    Fixad databas för klient

--- client/src/dbtest.py
+++ client/src/dbtest.py
@@ -8,12 +8,14 @@ from class_.base_objects import Employee, StatusCode, Mission
 #db.add(StatusCode('Done'))
 #db.add(Mission('Brunka', 12.2, 13.4, 0.0, 1))
 #olle = Employee('00:00:00:00:00', 'Olle', 'Karlsson')
-#db.add(olle)
+#db.add(olle) 
 
 #db.add(Employee('AA:AA:AA:AA:AA', 'MONGO', 'Svensson'))
 #db.add(StatusCode('Testing'))
-#db.add(Mission('Se pa film', 0.0, 0.0, 0.0, 2))
+#db.add(StatusCode('Inkommet larm'))
+#db.add(StatusCode('Utryckning!'))
+#db.add(Mission('Rädda katten i trädet', 15.578, 58.4048, 0.0, 2))
 
-#print db.get_one(Employee)
-for employee in db.get_all(Employee):
-    print employee
+print db.get_one(Mission).title
+#for mission in db.get_all(Mission):
+#    print mission
