2e7c2ace4e91f1aedd1340b42fe6c7b99273807f
maemo <maemo@maemo-desktop.(none)>
2010-11-23 16:49:37 +0100

    secondary server database update

--- server/src/requesthandler.py
+++ server/src/requesthandler.py
@@ -7,11 +7,15 @@ Created on Nov 11, 2010
 import network.networkcomponent
 
 def request(data, type, db):
+    """
+    Does different things depending on the datatype-object
+    """
     print 'request starts!'
-    if (type == 'db_add_or_update'):
+    if (type == 'db_add_or_update'): #Updating database
         print 'type = db_add_or_update'
         db.add_or_update(data)
-    elif (type == 'textMessage'):
+        networkcomponent.send(SETTINGS.secondary_ip, data, type) #Updating the second server
+    elif (type == 'textMessage'): #Sending message
         print 'type = textMessage'
         txtmsg = data
         network.networkcomponent.send(txtmsg.dst, txtmsg, 'textMessage')
