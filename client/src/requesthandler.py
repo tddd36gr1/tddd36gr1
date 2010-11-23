c26e998225e3eb76da5ac01386ada06e5fca5534
maemo <maemo@maemo-desktop.(none)>
2010-11-23 14:49:59 +0100

    requesthandler and baseobjects updated

--- client/src/requesthandler.py
+++ client/src/requesthandler.py
@@ -4,9 +4,13 @@ Created on Nov 11, 2010
 
 @author: alek
 '''
+import network.networkcomponent
+from class_.base_objects import TextMessage
 
 def request(data, type, db):
     print 'request starts!'
-    if (type == 'db_add_or_update'):
+    if (type == 'db_add_or_update' or type == 'textMessage'):
         print 'type correct'
         db.add_or_update(data)
+        
+        
