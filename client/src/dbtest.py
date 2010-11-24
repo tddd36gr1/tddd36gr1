54ce6a50f640de4c8e08d2b2e2ffdbb89439e79a
Linus Andersson <linus@b-77-46.eduroam.liu.se>
2010-11-24 09:49:10 +0100

    går att lägga till beskrivning till mission. Men vet inte om append-funktionen fungerar som den skall

--- client/src/dbtest.py
+++ client/src/dbtest.py
@@ -2,16 +2,17 @@
 """ EXEMPELKOD för hur man kan använda databasen """
 
 from db import DatabaseWorker
-from class_.base_objects import Employee, StatusCode, Mission
+from class_.base_objects import *
 
 db = DatabaseWorker()
 
 #db.add_or_update(StatusCode('Inkommet larm'))
 #db.add_or_update(StatusCode('Utryckning!'))
 #db.add_or_update(StatusCode('Slutfort'))
-#db.add_or_update(Mission('Brunka', 15.578, 58.4048, 0.0, 1))
-#db.add_or_update(Mission('Bajsa', 15.578, 58.4048, 0.0, 2))
-#db.add_or_update(Mission('Runka', 15.578, 58.4048, 0.0, 3))
+#db.add_or_update(Mission('Brunka', 34.578, 60.4048, 0.0, 1))
+db.add_or_update(Mission('Bajsa', 15.278, 58.5048, 0.0, 2, 'A cinderella story'))
+db.add_or_update(Mission('StorBrunk', 15.568, 58.4058, 0.0, 3, 'Du ska kora tills det svartnar'))
+db.add_or_update(Mission('Klippa Wibbe', 15.562, 58.4052, 0.0, 1, 'Cutta han!'))
 
 #for i in range(1, 10):
 #    db.add_or_update(Mission('Radda katten i tradet', 15.578, 58.4048, 0.0, 2))
@@ -20,9 +21,15 @@ db = DatabaseWorker()
 
 for mission in db.get_all(Mission):
     print mission
-
+"""
 for statuscode in db.get_all(StatusCode):
     print statuscode
     
 for employee in db.get_all(Employee):
     print employee
+"""
+#db.add(MissionText('y0 fett0'))
+for hehe in db.get_all(MissionText):
+    print hehe
+    
+    
