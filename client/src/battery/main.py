38636aa62d527e7a89e55678db5617140fd47b2a
maemo <maemo@maemo-desktop.(none)>
2010-12-03 12:15:56 +0100

    batterysaving mode working, monitorlight getting lower when battery reach 25 %

--- client/src/battery/main.py
+++ client/src/battery/main.py
@@ -1,4 +1,5 @@
 import dbus
+import os
 
 """
 Function that return an int value of the clients battery percent
@@ -10,3 +11,18 @@ def getBatteryPercent():
     uids = hal.FindDeviceByCapability('battery')
     dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
     return dev_obj.GetProperty('battery.charge_level.percentage')
+
+"""
+Function that set the brightness of the screen to 50
+(values between 0-251 could be set)
+"""
+def changeBrightness():
+    os.system('chroot /mnt/initfs dsmetest -l 50')
+
+"""
+If the batterypercent getting lower than 26, calling changeBrightness
+"""
+while 1:
+    if (getBatteryPercent() < 26):
+        changeBrightness()
+        break
