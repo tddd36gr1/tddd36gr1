9182747a399ff70f099513beaccdcbc04a4c02b4
maemo <maemo@maemo-desktop.(none)>
2010-12-06 13:43:57 +0100

    battery.py tillagd

---
+++ client/src/battery.py
@@ -0,0 +1,54 @@
+import dbus
+import os
+import time
+import threading
+
+"""
+Function that return an int value of the clients battery percent
+"""
+
+class Battery_check(threading.Thread):
+    
+    def __init__(self):
+        threading.Thread.__init__(self)
+    
+    def getBatteryPercent(self):
+        bus = dbus.SystemBus()
+        hal_obj = bus.get_object ('org.freedesktop.Hal', '/org/freedesktop/Hal/Manager')
+        hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
+        uids = hal.FindDeviceByCapability('battery')
+        dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
+        return dev_obj.GetProperty('battery.charge_level.percentage')
+    
+    """
+    Functions that set the brightness of the screen to a
+    value between 0-251
+    """
+    def lowerBrightness(self):
+        os.system('chroot /mnt/initfs dsmetest -l 50')
+        
+    def higherBrightness(self):
+        os.system('chroot /mnt/initfs dsmetest -l 250')
+    
+    def run(self):
+        """
+        If the batterypercent getting lower than 26, calling lowerBrightness
+        Else call higherBrightness
+        """
+        while 1:
+            time.sleep(60)
+            x = self.getBatteryPercent()
+            print x
+            if (x < 26):
+                self.lowerBrightness()
+            
+            elif (x > 26):
+                self.higherBrightness()
+                
+def start():
+    global battery_check
+    battery_check = Battery_check()
+    battery_check.start()
+    
+def getBatteryPercent():
+    return battery_check.getBatteryPercent()
