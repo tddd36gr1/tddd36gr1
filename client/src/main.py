3fc37438967c55225dc61fa97508169b226372fe
maemo <maemo@maemo-desktop.(none)>
2010-12-06 13:11:20 +0100

    ändrat så att battery är i en egen tråd och grejor!

--- client/src/main.py
+++ client/src/main.py
@@ -7,8 +7,12 @@ Created on 17 nov 2010
 
 from db import DatabaseWorker
 import network.networkcomponent as networkcomponent
+import battery
 import gui
+
+
 
 db = DatabaseWorker()
 networkcomponent.serverStart(db)
+battery.start()
 gui.start(db)
